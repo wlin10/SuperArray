ce86ad225ed8a3df8a51ea01c2b094232efc52ce
wlin10 <wlin10@stuy.edu>
2020-11-05 01:09:36 -0500

    toArray

--- SuperArray.java
+++ SuperArray.java
@@ -104,4 +104,12 @@ public class SuperArray {
     return ans;
   }
 
+  public String[] toArray() {
+    String[] ans = new String[size];
+    for (int i = 0; i < size; i++) {
+      ans[i] = data[i];
+    }
+    return ans;
+  }
+
 }
