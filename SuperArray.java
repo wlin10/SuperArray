64caed47f574c7f2859e011e8d32e19a8f0421e9
wlin10 <wlin10@stuy.edu>
2020-11-10 13:19:05 -0500

    initialCapacity

--- SuperArray.java
+++ SuperArray.java
@@ -66,6 +66,9 @@ public class SuperArray {
   }
 
   public SuperArray (int initialCapacity) {
+    if (initialCapacity < 0) {
+      throw new IllegalArgumentException("initialCapacity " + initialCapacity + " cannot be negative");
+    }
     data = new String[initialCapacity];
     size = 0;
   }
